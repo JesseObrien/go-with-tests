ede0096fa73bf1b5cc374e8f357ae3a3f2076ca0
Jesse O'Brien <jesse@jesse-obrien.ca>
2019-08-25 08:26:10 -0400

    Refactoring

--- hello.go
+++ hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello returns hello to the name
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
