8b938f3d045b2cc43f13a365cb87c32d060e0daa
Jesse O'Brien <jesse@jesse-obrien.ca>
2019-08-25 08:32:47 -0400

    Hello world tests complete

--- hello_test.go
+++ hello_test.go
@@ -12,16 +12,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Bob")
+		got := Hello("Bob", "")
 		want := "Hello, Bob"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jeese", "French")
+		want := "Bonjour, Jeese"
 		assertCorrectMessage(t, got, want)
 	})
 
