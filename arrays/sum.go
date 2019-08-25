2005eb7832f85eaf6cd6201b9e0db26d3ba8fd03
Jesse O'Brien <jesse@jesse-obrien.ca>
2019-08-25 19:24:55 -0400

    More summing

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,6 @@
 package arrays
 
-// Sum provides the sum total of an array of integers
+// Sum provides the sum total of a slice of integers
 func Sum(numbers []int) int {
 	sum := 0
 	for _, num := range numbers {
@@ -8,4 +8,30 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+// SumAll Provides the sum total of n slices of integers
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails sums only the same position in any slices
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,40 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 3, 6})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
 }
